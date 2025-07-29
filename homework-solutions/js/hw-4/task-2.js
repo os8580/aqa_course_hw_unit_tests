79257fe6dc78fe0fcd1a26bd24c521af3f5006fd
Oleg S. <kindovso@gmail.com>
2025-07-30 00:31:00 +0200

    fixed hw-4

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -7,7 +7,7 @@ const minAge = 18;
 // 2. Создать переменную "maxAge" и присвоить ей значение 60
 const maxAge = 60;
 // 3. Создать переменную "age", в которую вы будете вводить возраст при выполнении программы
-const age = 60;
+const age = 61;
 // 4. Добавьте проверку: если тип данных в переменной age не number - вывести в консоль Incorrect data type
 if (typeof age !== 'number') {
   console.log("Incorrect data type");
@@ -21,7 +21,7 @@ if (age < minAge) {
   console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
 } else if (age >= minAge && age < maxAge) {
   console.log("Welcome  !");
-} else if (age >= maxAge) {
+} else if (age > maxAge) {
   console.log("Keep calm and look Culture channel");
 } else {
   console.log("Technical work");
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -21,7 +21,7 @@ if (typeof age !== "number" || isNaN(age)) {
   console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
 } else if (age >= minAge && age < maxAge) {
   console.log("Welcome  !");
-} else if (age >= maxAge) {
+} else if (age > maxAge) {
   console.log("Keep calm and look Culture channel");
 } else {
   console.log("Technical work");
